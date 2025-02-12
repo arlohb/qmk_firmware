0e7abde1c46e41e6ee12774c1ab422ebbfc4679a
arlohb <arlo@urwick.co.uk>
2025-02-12 12:09:12 +0000

    Improved keymap

--- keyboards/sofle/keymaps/arlohb/keymap.c
+++ keyboards/sofle/keymaps/arlohb/keymap.c
@@ -85,8 +85,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT(
   _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL,  KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,      _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, UK_BSLS, _______,
+  _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  _______,  _______,  _______, _______, KC_LCBR, KC_RCBR, _______,      _______, KC_LBRC, KC_RBRC, _______, _______, UK_BSLS, _______,
                        _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
 ),
 /* RAISE
@@ -104,11 +104,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
-  _______, _______,  _______, _______,  _______, _______,                        _______, _______, _______, _______,  _______ ,_______,
-  _______, _______,  _______, _______,  _______, _______,                        KC_PLUS, KC_MINS, UK_UNDS,  KC_EQL,UK_HASH, KC_DEL,
-  _______, _______,  _______, _______,  _______, _______,                        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  _______, _______ ,
-  _______, C(KC_Z), C(KC_X), C(KC_C), C(KC_V), _______,  _______,       _______, _______, KC_LSTRT, _______, KC_LEND, _______, _______,
-                         _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                        _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______,                        KC_PLUS, KC_MINS, UK_UNDS,  KC_EQL, UK_HASH, KC_DEL ,
+  _______, _______, _______, _______, _______, _______,                        KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
+  _______, _______, _______, _______, _______, _______,  _______,     _______, KC_LBRC, KC_RBRC, _______, KC_LEND, _______, _______,
+                    _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______
 ),
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ADJUST] = LAYOUT(
   QK_BOOT , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX , XXXXXXX, CG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
   )
